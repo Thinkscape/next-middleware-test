46d6250368127d8f6a8a6ea03db97c84929ef4d0
Arthur Bodera <abodera@gmail.com>
2023-12-01 12:37:05 +1100

    Try this for repro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { getCookie } from "@/lib/cookie-util";
 
+// Try with this
+export const runtime = 'edge';
+
 export default async function Test() {
   const test = getCookie("test");
   console.log("page:", test);
